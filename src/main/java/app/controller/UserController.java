dd471da983cc070db5a3fcca58415a5c38d00337
Alex Balak <alex.balak@outlook.com>
2025-06-10 23:21:37 +0200

    minimal signup app (Not tested yet)

---
+++ src/main/java/app/controller/UserController.java
@@ -0,0 +1,27 @@
+package app.controller;
+
+import app.model.User;
+import app.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
--- src/main/java/app/repository/UserRepository.java
+++ src/main/java/app/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package app.repository;
 
-public class UserRepository {
+import app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username); // Returns a single user wrapped in Optional
 }
